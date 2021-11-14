b90cd10eecca4946ca2e6e3b61b01d9b18c56745
Delowar Hossain <delowardev@gmail.com>
2021-11-15 00:36:10 +0600

    Draft

---
+++ src/components/canvas.jsx
@@ -0,0 +1,7 @@
+export default function Canvas () {
+    return (
+        <div className="app-canvas">
+            App canvas
+        </div>
+    )
+}
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import Canvas from "./components/canvas";
+
+function App() {
+
+  return (
+      <Canvas />
+  )
+
+}
+
+export default App
